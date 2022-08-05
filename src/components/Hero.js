4ccff673780be71bf0d9c53c8de66fbac5af0d24
Ryan Howard <ryanghoward@protonmail.com>
2022-08-05 15:02:50 -0700

    Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,12 @@
 import VisibilitySensor from "react-visibility-sensor";
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+import backgraphics from "../images/backgraphics.png";
+import p1 from "../images/p 1.png";
+import p2 from "../images/p 2.png";
+import p3 from "../images/p 3.png";
+import p4 from "../images/p 4.png";
 
 function Hero() {
   const [elementIsVisible, setElementIsVisible] = useState(false);
@@ -61,7 +68,45 @@ function Hero() {
             <span className='text-[13px]'>Download now on IOS and Android</span>
           </div>
         </div>
-        <div className='images relative w-[50%]'></div>
+        <div className='images relative w-[50%]'>
+          <motion.img
+            variants={bg}
+            animate={`${elementIsVisible}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={backgraphics}
+            alt='/'
+            className='absolute top-[-8rem] left-[19rem]'
+          />
+          <img
+            src={p1}
+            alt='/'
+            className='absolute top-[-15rem] h-[34rem] left-[13rem]'
+          />
+          <motion.img
+            variants={musicPlayer}
+            animate={`${elementIsVisible}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={p2}
+            alt='/'
+            className='absolute left-[235px] top-[94px] w-[175px]'
+          />
+          <motion.img
+            variants={rect}
+            animate={`${elementIsVisible}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={p3}
+            alt='/'
+            className='absolute w-[5rem] left-[13rem] top-[12rem]'
+          />
+          <motion.img
+            variants={heart}
+            animate={`${elementIsVisible}`}
+            transition={{ duration: 1, type: "ease-out" }}
+            src={p4}
+            alt='/'
+            className='absolute w-[5rem] left-[12.5rem] top-[12rem]'
+          />
+        </div>
       </div>
     </VisibilitySensor>
   );
