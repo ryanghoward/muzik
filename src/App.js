2709836a6e92eff5d006c09bb60747da3114615f
Ryan Howard <ryanghoward@protonmail.com>
2022-08-05 14:23:32 -0700

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>Bonjour</div>;
+  return <div className='text-3xl font-bold underline'>Bonjour</div>;
 }
 
 export default App;
